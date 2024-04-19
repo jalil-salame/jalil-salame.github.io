b8d251ec4577abbae4b7e4b06d854d0f2c2b8239
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-04-19 21:21:31 +0200

    chore: update flake.lock
    • Updated input 'flake-utils':
        'github:numtide/flake-utils/cfacdce06f30d2b68473a46042957675eebb3401' (2023-04-11)
      → 'github:numtide/flake-utils/b1d9ab70662946ef0850d488da1c9019f3a9752a' (2024-03-11)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/897876e4c484f1e8f92009fd11b7d988a121a4e7' (2023-05-06)
      → 'github:NixOS/nixpkgs/66adc1e47f8784803f2deb6cacd5e07264ec2d5c' (2024-04-16)

--- flake.nix
+++ flake.nix
@@ -6,62 +6,64 @@
   inputs.kangae.flake = false;
   inputs.kangae.url = "github:ayushnix/kangae";
 
-  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-  inputs.pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
-
-  outputs = { self, nixpkgs, kangae, pre-commit-hooks }:
-    let
-      # Helpers for producing system-specific outputs
-      supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
-      forEachSupportedSystem' = f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; inherit system; });
-      forEachSupportedSystem = f: forEachSupportedSystem' ({ pkgs, ... }: f pkgs);
-      themeName = (builtins.fromTOML (builtins.readFile "${theme}/theme.toml")).name;
-      theme = kangae;
-    in
-    {
-      checks = forEachSupportedSystem' ({ pkgs, system }: {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = builtins.path { path = ./.; name = "configuration.nix"; };
-          hooks.typos.enable = true;
-          hooks.nixpkgs-fmt.enable = true;
-        };
-      });
-      packages = forEachSupportedSystem (pkgs:
-        let
-          site = pkgs.stdenv.mkDerivation {
-            pname = "jalil-salame.github.io";
-            version = "2024-02-10";
-            src = builtins.path { path = ./.; name = "blog"; };
-            nativeBuildInputs = [ pkgs.zola ];
-            configurePhase = ''
-              mkdir -p 'themes/${themeName}'
-              cp -r ${theme}/* 'themes/${themeName}'
-            '';
-            buildPhase = "zola build --output-dir $out";
-          };
-        in
-        {
-          inherit site;
-          default = site;
+  outputs = {
+    self,
+    nixpkgs,
+    kangae,
+  }: let
+    # Helpers for producing system-specific outputs
+    supportedSystems = ["x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux"];
+    forEachSupportedSystem' = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+          inherit system;
         });
-      formatter = forEachSupportedSystem (pkgs: pkgs.nixpkgs-fmt);
-      apps = forEachSupportedSystem' ({ pkgs, system }:
-        let
-          site = self.packages.${system}.site;
-          serve = pkgs.writeShellScriptBin "pages" "${pkgs.miniserve}/bin/miniserve --interfaces=127.0.0.1 --index ${site}/index.html ${site}";
-        in
-        {
-          default.type = "app";
-          default.program = "${serve}/bin/pages";
-        });
-      devShells = forEachSupportedSystem' ({ pkgs, system }: {
-        default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.site ];
-          shellHook = ''
-            mkdir -p themes
-            ln -snf '${theme}' 'themes/${themeName}'
-          '' + self.checks.${system}.pre-commit-check.shellHook;
+    forEachSupportedSystem = f: forEachSupportedSystem' ({pkgs, ...}: f pkgs);
+    themeName = (builtins.fromTOML (builtins.readFile "${theme}/theme.toml")).name;
+    theme = kangae;
+  in {
+    packages = forEachSupportedSystem (pkgs: let
+      site = pkgs.stdenv.mkDerivation {
+        pname = "jalil-salame.github.io";
+        version = "2024-02-10";
+        src = builtins.path {
+          path = ./.;
+          name = "blog";
         };
-      });
-    };
+        nativeBuildInputs = [pkgs.zola];
+        configurePhase = ''
+          mkdir -p 'themes/${themeName}'
+          cp -r ${theme}/* 'themes/${themeName}'
+        '';
+        buildPhase = "zola build --output-dir $out";
+      };
+    in {
+      inherit site;
+      default = site;
+    });
+    formatter = forEachSupportedSystem (pkgs: pkgs.nixpkgs-fmt);
+    apps = forEachSupportedSystem' ({
+      pkgs,
+      system,
+    }: let
+      site = self.packages.${system}.site;
+      serve = pkgs.writeShellScriptBin "pages" "${pkgs.miniserve}/bin/miniserve --interfaces=127.0.0.1 --index ${site}/index.html ${site}";
+    in {
+      default.type = "app";
+      default.program = "${serve}/bin/pages";
+    });
+    devShells = forEachSupportedSystem' ({
+      pkgs,
+      system,
+    }: {
+      default = pkgs.mkShell {
+        inputsFrom = [self.packages.${system}.site];
+        shellHook = ''
+          mkdir -p themes
+          ln -snf '${theme}' 'themes/${themeName}'
+        '';
+      };
+    });
+  };
 }
