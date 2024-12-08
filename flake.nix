b8907ce1c9a497671ebafdee7f6a8c49f59565c1
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-12-08 15:25:54 +0100

    feat(flake): use flake-parts and treefmt-nix

--- flake.nix
+++ flake.nix
@@ -1,70 +1,69 @@
 {
   description = "My Blog/Website using Zola";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
+  };
 
   outputs =
     inputs:
-    let
-      # Helpers for producing system-specific outputs
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      forEachSupportedSystem =
-        f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (system: f (inputs.nixpkgs.legacyPackages.${system}));
-      themeName = (builtins.fromTOML (builtins.readFile ./config.toml)).theme;
-      theme' = pkgs: (pkgs.callPackage ./themes.nix { }).${themeName};
-      site' =
-        pkgs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        { pkgs, ... }:
         let
-          theme = theme' pkgs;
-        in
-        pkgs.callPackage ./default.nix { inherit theme themeName; };
-    in
-    {
-      packages = forEachSupportedSystem (
-        pkgs:
-        let
-          site = site' pkgs;
+          themeName = (builtins.fromTOML (builtins.readFile ./config.toml)).theme;
+          theme = (pkgs.callPackage ./themes.nix { }).${themeName};
+          site = pkgs.callPackage ./default.nix { inherit theme themeName; };
         in
         {
-          inherit site;
-          default = site;
-        }
-      );
-      apps = forEachSupportedSystem (pkgs: {
-        default =
-          let
-            script = pkgs.writers.writeDash "pages" ''
-              ${pkgs.miniserve}/bin/miniserve \
-                  --interfaces=127.0.0.1 \
-                  --index ${site' pkgs}/index.html \
-                  ${site' pkgs}
-            '';
-          in
-          {
-            type = "app";
-            program = "${script}";
+          packages = {
+            inherit site;
+            default = site;
           };
-      });
-      devShells = forEachSupportedSystem (
-        pkgs:
-        let
-          site = site' pkgs;
-          theme = theme' pkgs;
-        in
-        {
-          default = pkgs.mkShell {
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              statix.enable = true;
+              typos.enable = true;
+            };
+            settings.formatter.typos.excludes = [ "static/*" ];
+          };
+
+          apps.default =
+            let
+              script = pkgs.writers.writeDash "pages" ''
+                ${pkgs.miniserve}/bin/miniserve \
+                    --interfaces=127.0.0.1 \
+                    --index ${site}/index.html \
+                    ${site}
+              '';
+            in
+            {
+              type = "app";
+              program = "${script}";
+            };
+
+          devShells.default = pkgs.mkShell {
             inputsFrom = [ site ];
             shellHook = ''
               mkdir -p themes
-              ln -sn '${theme}' 'themes/${themeName}'
+              ln -sn ${theme} themes/${themeName}
             '';
           };
-        }
-      );
-      formatter = forEachSupportedSystem (pkgs: pkgs.nixfmt-rfc-style);
+        };
     };
 }
